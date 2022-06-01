7d15d109c7cbeaa237f60d37d5cd0009307fa56a
s0hamn <soham.naigaonkar@gmail.com>
2022-06-01 22:27:09 +0530

    .

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ let loader1 = document.getElementById("loader1")
 let homeSliderWrapper = document.getElementById("home-slider-wrapper")
 let navA = document.getElementsByClassName("nav-a")
 let genreItems = document.getElementsByClassName("genre-item")
+let searchBtn = document.getElementsByClassName("search-btn")[0]
+let searchText = document.getElementsByClassName('search-text')[0]
+
 
 const BASE_IMG_URL = "https://image.tmdb.org/t/p/w500"
 const BASE_GENRE_URL = "https://api.themoviedb.org/3/discover/movie?api_key=53429b93896ec0365c0d076f33deebb1&with_genres="
+const BASE_SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=53429b93896ec0365c0d076f33deebb1&query="
 const trendingURL = "https://api.themoviedb.org/3/trending/all/day?api_key=53429b93896ec0365c0d076f33deebb1"
 
 const baseLatestURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=53429b93896ec0365c0d076f33deebb1&language=en-US&page='
@@ -65,7 +69,7 @@ async function fetchLatestMovies(pageCount) {
       title = currMovie.name;
     }
 
-    let type = "MOVIE" 
+    let type = "MOVIE"
 
     str += `<div class="box">
     <img src="${imgUrl}" alt="">
@@ -122,18 +126,63 @@ async function startHomeSlider() {
   homeSliderWrapper.innerHTML = str;
 
 }
+async function fetchSearchData(query) {
 
-async function fetchGenreData(genre){
+  let searchURL = BASE_SEARCH_URL + `${query}&page=1`
+  let response = await fetch(searchURL);
+  let data = await response.text();
+  let results = JSON.parse(data).results;
+  let str = ``
+
+  Array.from(results).forEach(currMovie => {
+    let imgUrl = BASE_IMG_URL + currMovie.poster_path;
+    let rating = currMovie.vote_average;
+    let title = currMovie.title;
+
+    if (title == undefined) {
+      title = currMovie.name;
+    }
+
+    let type = "MOVIE"
+
+    str += `<div class="box">
+    <img src="${imgUrl}" alt="IMAGE NOT FOUND">
+
+
+    <div class="box-text">
+        <h2>${title}</h2>
+        <h3>${type}</h3>
+        <div class="play-movie">
+            <div class="rating">
+                <i class='bx bxs-star'></i>
+                <span>${rating}</span>
+
+            </div>
+            <a href="#" class="box-btn"><i class='bx bx-play-circle'></i></a>
+        </div>
+    </div>
+</div>`
+  })
+
+  document.getElementsByClassName("search")[0].style.display = "block"
+  document.getElementById("main-content").style.display = "none"
+  searchContent.innerHTML = str
+
+
+
+}
+
+async function fetchGenreData(genre) {
   const genres = {
-    "Comedy":35,
-    "Action":28,
-    "Adventure":12,
-    "Animation":16,
-    "Crime":80,
-    "Fantasy":14,
-    "Horror":27,
-    "Romance":10749,
-    "Thriller":53
+    "Comedy": 35,
+    "Action": 28,
+    "Adventure": 12,
+    "Animation": 16,
+    "Crime": 80,
+    "Fantasy": 14,
+    "Horror": 27,
+    "Romance": 10749,
+    "Thriller": 53
   }
 
   let genreID = genres[genre];
@@ -153,7 +202,7 @@ async function fetchGenreData(genre){
       title = currMovie.name;
     }
 
-    let type = "MOVIE" 
+    let type = "MOVIE"
 
     str += `<div class="box">
     <img src="${imgUrl}" alt="">
@@ -175,20 +224,39 @@ async function fetchGenreData(genre){
   })
 
   genreContent.innerHTML = str;
-  
 
-  
+
+
 }
 
 let trendingSlider = document.getElementById("trending-slider")
 let newContent = document.getElementById("new-content")
 let genreContent = document.getElementById("genre-content")
+let searchContent = document.getElementById("search-content")
 let nextPageBtn = document.getElementById("next-page-button")
 const trendingMoviesList = [];
 menu.onclick = () => {
   menu.classList.toggle('move')
   navbar.classList.toggle('active')
 }
+
+let isSearching = false
+searchBtn.addEventListener('click', () => {
+  searchText.classList.add("search-active")
+  searchText.focus()
+  if (isSearching) {
+    console.log(searchText.value)
+    if (searchText.value.length > 3) {
+      fetchSearchData(searchText.value);
+    }
+    else {
+      alert("Please enter a search greater than 3")
+    }
+  }
+  else {
+    isSearching = true
+  }
+})
 
 Array.from(navA).forEach(element => {
   element.addEventListener('click', () => {
@@ -197,20 +265,20 @@ Array.from(navA).forEach(element => {
   })
 })
 function resetHighlightGenre() {
-  Array.from(genreItems).forEach(element =>{
+  Array.from(genreItems).forEach(element => {
     element.classList.remove("highlight-genre")
   })
 }
 
-Array.from(genreItems).forEach(element =>{
-  element.addEventListener('click', ()=>{
+Array.from(genreItems).forEach(element => {
+  element.addEventListener('click', () => {
     resetHighlightGenre()
     genreContent.innerHTML = `<div class="loader2">
   <div class="loader-icon"></div>
 </div>`;
 
     element.classList.toggle('highlight-genre')
-    
+
     fetchGenreData(element.innerText);
   })
 })
@@ -263,8 +331,8 @@ var swiper = new Swiper(".trending-content", {
     disableOnInteraction: false
   },
   breakpoints: {
-    300:{
-      slidesPerView:2,
+    300: {
+      slidesPerView: 2,
       spaceBetween: 10,
     },
     640: {
